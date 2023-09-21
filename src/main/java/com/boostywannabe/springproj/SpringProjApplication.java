45ab4d60a920441960fd0e6e0a81354f30e6a70d
MrRetro <gleb2super@gmail.com>
2023-09-21 20:07:28 +0300

    added swagger

--- src/main/java/com/boostywannabe/springproj/SpringProjApplication.java
+++ src/main/java/com/boostywannabe/springproj/SpringProjApplication.java
@@ -1,9 +1,7 @@
 package com.boostywannabe.springproj;
 
-import com.boostywannabe.springproj.model.ImageSpan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class SpringProjApplication {
